d2a13039e5e4152244ca7889fcd28a018f5c7709
Nicolas <santanalopesnicolas@gmail.com>
2025-11-18 14:23:02 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,14 +8,24 @@ def index():
     return render_template("index.html")
 
 #Primeira Rota é soma
-
-
+@app.route("/soma")
+def soma():
+    v1 = float(request.args.get("valor1",0))
+    v2 = float(request.args.get("valor2",0))
+    return {"resultado": v1 + v2}
 # Segunda é subtrair 
-
+@app.route("/subtrair")
+def subtrair():
+    v1 = float(request.args.get("valor",0))
+    v2 = float(request.args.get("valor2",0))
+    return {"resultado": v1 - v2}
 
 # Terceira é multiplicar
-
-
+@app.route("/multiplicar")
+def multiplicar():
+    v1 = float(request.args.get("valor1",0))
+    v2 = float(request.args.get("valor2",0))
+    return {"resultado": v1 * v2}
 
 # Terceira é dividir 
 
